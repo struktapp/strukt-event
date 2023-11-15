69a971b49fdd2a74759ccaa9bb0f6ffe17d7af72
samweru <samweru@gmail.com>
2023-11-15 22:00:56 +0300

    alias for loop func keys

--- src/Strukt/Loop.php
+++ src/Strukt/Loop.php
@@ -5,6 +5,7 @@ namespace Strukt;
 class Loop extends Cmd{
 
 	private static $halted = false;
+	private static $halted_at = null;
 
 	private static function noExit(){
 
@@ -14,8 +15,9 @@ class Loop extends Cmd{
 	public static function halt(callable $callback){
 
 		static::$halted = true;
+		static::$halted_at = sprintf("init:halted@%s", sha1(rand()));
 
-		static::add(sprintf("init:halted@%s", sha1(rand())), $callback);
+		static::add(static::$halted_at, $callback);
 	}
 
 	public static function pause(bool $halt = true){
@@ -30,13 +32,18 @@ class Loop extends Cmd{
 
 	public static function run(array $options = []){
 
-		if(array_key_exists("alias", $options))
-			static::$names = alias(sprintf("%s*", trim($options["alias"],"*")));
+		$names = static::$names;
+		if(array_key_exists("alias", $options)){
+
+			$aliases = alias(sprintf("%s*", trim($options["alias"],"*")));
+			$names = arr($aliases)->each(fn($k, $alias)=>alias($alias))->yield();
+			$names[] = static::$halted_at;
+		}
 
 		$idx = 0;
 	    while(static::noExit()){
 
-	    	$name = static::$names[$idx];
+	    	$name = trim($names[$idx]);
 	    	$args = static::$args[$name];
 
 	    	static::exec($name, $args);
